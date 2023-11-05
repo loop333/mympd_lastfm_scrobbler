1cc32947d3a2d911ae6c3f78532897d3c857c15e
Dmitry <37012903+loop333@users.noreply.github.com>
2023-11-05 16:43:59 +0500

    Update and rename lastfm_config to lastfm_config.lua

---
+++ lastfm_config.lua
@@ -0,0 +1,14 @@
+-- {"order":1,"arguments":[]}
+lastfm_config = {}
+
+lastfm_config.URL_API     = "https://ws.audioscrobbler.com/2.0/?format=json"
+
+lastfm_config.USERNAME    = YOUR_USERNAME
+lastfm_config.PASSWORD    = YOUR_PASSWORD
+lastfm_config.API_KEY     = YOUR_API_KEY
+lastfm_config.SECRET      = YOUR_SECRET
+
+-- use script lastfm_get_session_key.lua
+lastfm_config.SESSION_KEY = YOUR_SESSION_KEY
+
+return lastfm_config
